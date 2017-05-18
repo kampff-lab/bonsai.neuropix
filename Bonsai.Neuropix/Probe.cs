2866677c911e3ea4e88fabe43c7a7a91a1c2ba8b
glopes <glopesdev@users.noreply.github.com>
2017-05-18 11:48:19 +0100

    Reorganized probe source properties and configuration categories.

--- Bonsai.Neuropix/Probe.cs
+++ Bonsai.Neuropix/Probe.cs
@@ -1,7 +1,10 @@
-﻿using Neuropix.Net;
+﻿using Bonsai.IO;
+using Neuropix.Net;
 using OpenCV.Net;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
 using System.Linq;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
@@ -12,17 +15,56 @@ namespace Bonsai.Neuropix
 {
     public class Probe : Source<NeuropixDataFrame>
     {
-        public string FileName { get; set; }
+        public Probe()
+        {
+            StreamingFileName = "datalog.npx";
+        }
 
-        public string GainCalibration { get; set; }
+        [Category("Acquisition")]
+        [Description("Indicates whether to stream all acquisition data to a file.")]
+        public bool FileStreaming { get; set; }
 
+        [Category("Acquisition")]
+        [FileNameFilter("NPX Files (*.npx)|*.npx|All files (*.*)|*.*")]
+        [Description("The path to the NPX file used to record all acquisition data.")]
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", typeof(UITypeEditor))]
+        public string StreamingFileName { get; set; }
+
+        [Category("Acquisition")]
+        [Description("The optional suffix used to generate streaming file names.")]
+        public PathSuffix StreamingSuffix { get; set; }
+
+        [Category("Configuration")]
+        [Description("The high-pass filter bandwidth used during acquisition of AP data.")]
+        public FilterBandwidth FilterBandwidth { get; set; }
+
+        [Category("Configuration")]
+        [Description("Indicates whether the headstage LEDs should be turned off.")]
+        public bool LedOff { get; set; }
+
+        [Category("Calibration")]
+        [FileNameFilter("CSV Files (*.csv)|*.csv|All files (*.*)|*.*")]
+        [Description("The path to a CSV file containing ADC comparator calibration data.")]
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", typeof(UITypeEditor))]
         public string ComparatorCalibration { get; set; }
 
+        [Category("Calibration")]
+        [FileNameFilter("CSV Files (*.csv)|*.csv|All files (*.*)|*.*")]
+        [Description("The path to a CSV file containing ADC offset calibration data.")]
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", typeof(UITypeEditor))]
         public string AdcOffsetCalibration { get; set; }
 
+        [Category("Calibration")]
+        [FileNameFilter("CSV Files (*.csv)|*.csv|All files (*.*)|*.*")]
+        [Description("The path to a CSV file containing ADC slope calibration data.")]
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", typeof(UITypeEditor))]
         public string AdcSlopeCalibration { get; set; }
 
-        public FilterBandwidth FilterBandwidth { get; set; }
+        [Category("Calibration")]
+        [FileNameFilter("CSV Files (*.csv)|*.csv|All files (*.*)|*.*")]
+        [Description("The path to a CSV file containing gain correction data.")]
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", typeof(UITypeEditor))]
+        public string GainCalibration { get; set; }
 
         public override IObservable<NeuropixDataFrame> Generate()
         {
@@ -70,16 +112,18 @@ namespace Bonsai.Neuropix
                         else basestation.ApplyGainCalibrationFromEeprom();
                         Console.WriteLine("OK");
 
-                        basestation.LedOff(true);
+                        basestation.LedOff(LedOff);
                         basestation.Mode = AsicMode.Recording;
                         basestation.DataMode = true;
                         basestation.TriggerMode = false;
                         basestation.SetNrst(false);
                         basestation.ResetDatapath();
-                        var fileName = FileName;
-                        if (!string.IsNullOrEmpty(fileName))
+                        var fileName = StreamingFileName;
+                        if (!string.IsNullOrEmpty(fileName) && FileStreaming)
                         {
-                            basestation.StartRecording(FileName);
+                            PathHelper.EnsureDirectory(fileName);
+                            fileName = PathHelper.AppendSuffix(fileName, StreamingSuffix);
+                            basestation.StartRecording(StreamingFileName);
                         }
                         Console.WriteLine("Neuropix recording armed.");
 
@@ -100,7 +144,7 @@ namespace Bonsai.Neuropix
                         }
 
                         Console.WriteLine("Neuropix stop recording...");
-                        if (!string.IsNullOrEmpty(fileName))
+                        if (!string.IsNullOrEmpty(fileName) && FileStreaming)
                         {
                             basestation.StopRecording();
                         }
